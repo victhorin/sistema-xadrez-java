e733c29e0dfa79fa79a8ee3057d49cab95153374
victor rib <victor.rib66@gmail.com>
2024-08-28 20:06:45 -0300

    Adição da classe Exceção e verificações na classe Tabuleiro

--- src/jogoTabuleiro/Tabuleiro.java
+++ src/jogoTabuleiro/Tabuleiro.java
@@ -6,6 +6,9 @@ public class Tabuleiro {
 	private Peca[][] pecas;
 
 	public Tabuleiro(int linhas, int colunas) {
+		if (linhas < 1 || colunas < 1) {
+			throw new ExcecaoTabuleiro("Erro ao criar tabuleiro: é necessário haver ao menos 1 linha e coluna");
+		}
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
@@ -15,28 +18,45 @@ public class Tabuleiro {
 		return linhas;
 	}
 
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
-
 	public int getColunas() {
 		return colunas;
 	}
 
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
-
 	public Peca peca(int linha, int coluna) {
+		if (!posicaoExiste(linha, coluna)) {
+			throw new ExcecaoTabuleiro("Essa posição não existe no tabuleiro");
+		}
 		return pecas[linha][coluna];
 	}
 
 	public Peca peca(Posicao posicao) {
+		if (!posicaoExiste(posicao)) {
+			throw new ExcecaoTabuleiro("Essa posição não existe no tabuleiro");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 
 	public void localPeca(Peca peca, Posicao posicao) {
+		if (existeUmaPeca(posicao)) {
+			throw new ExcecaoTabuleiro("Já existe uma peça nessa posição " + posicao);
+		}
+
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
+	}
+
+	private boolean posicaoExiste(int linha, int coluna) {
+		return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+	}
+
+	public boolean posicaoExiste(Posicao posicao) {
+		return posicaoExiste(posicao.getLinha(), posicao.getColuna());
+	}
+
+	public boolean existeUmaPeca(Posicao posicao) {
+		if (!posicaoExiste(posicao)) {
+			throw new ExcecaoTabuleiro("Essa posição não existe no tabuleiro");
+		}
+		return peca(posicao) != null;
 	}
 }
